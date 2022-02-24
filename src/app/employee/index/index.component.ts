f06d023c7bee5f8ee9e191ded71c40782a10d799
Abdellah <abdellahbukadduh99@gmail.com>
2022-02-24 21:40:57 +0100

    Agregada algo de documentación de la vista index

--- src/app/employee/index/index.component.ts
+++ src/app/employee/index/index.component.ts
@@ -10,9 +10,9 @@ import { Employee } from '../employee';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-  employees: Employee[] = [];
-  page: number = 1;
-  loading: boolean;
+  employees: Employee[] = []; //Array de tipo Employee, que contendrá los registros traidos de la api
+  page: number = 1; //Página inicial de la paginación, lo normal es que sea "1"
+  loading: boolean; //Esta variable indica si se están cargando datos, mientras no termine la carga permanecerá en verdadero
 
 
   // constructor() { }
@@ -23,11 +23,13 @@ export class IndexComponent implements OnInit {
   ngOnInit(): void {
     this.employeeService.getAll().subscribe((data: Employee[])=>{
       this.employees = data;
-      this.loading = false;
+      this.loading = false; //Una vez que tengo los datos cambio este valor para dejar de mostrar la animación de carga
       console.log(this.employees);
     })
   }
 
+
+//Elimino el registro con el id que se pasa como parámetro
   deleteEmployee(id:number){
     this.employeeService.delete(id).subscribe(res => {
          this.employees = this.employees.filter(item => item.id !== id);
